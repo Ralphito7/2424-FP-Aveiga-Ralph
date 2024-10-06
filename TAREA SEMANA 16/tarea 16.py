bed2cebd37ca4241270add7a2d7cb906612d8b5b
Ralphito7 <ralph.aveiga@gmail.com>
2024-10-05 22:30:27 -0500

    Trabajo con Archivos de Texto en Python

---
+++ TAREA SEMANA 16/tarea 16.py
@@ -0,0 +1,19 @@
+# 1. Definimos el nombre del archivo
+file_name = "my_notes.txt"
+# Abrimos el archivo en modo escritura (esto crea el archivo)
+archivo_escritura = open(file_name, "w")
+# Escribimos algunas líneas en el archivo
+archivo_escritura.write("Línea 1: Soy muy extrovertido\n")
+archivo_escritura.write("Línea 2: me obsesiona el basket\n")
+#También podemos escribir varias líneas a la vez
+lineas = ["Línea 3: jugar play station me entretiene\n", "Línea 4: boxear me desestresa\n"]
+archivo_escritura.writelines(lineas)
+#Cerramos el archivo de escritura
+archivo_escritura.close()
+#Ahora, abrimos el archivo en modo lectura para leer lo que escribimos
+archivo_lectura = open(file_name, "r")
+#Leemos y mostramos cada línea en la consola
+for line in archivo_lectura:
+    print(line.strip())  # Usamos strip() para quitar el salto de línea al final
+#Cerramos el archivo de lectura
+archivo_lectura.close()
